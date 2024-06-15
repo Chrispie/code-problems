764bad1eb76fa27847ab5535718ccd80f51993c5
chrispie <chrispie123@gmail.com>
2024-06-16 00:02:11 +0200

    Adding sample files for problem adding h2 console to quarkus project.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# code-problems
+# What do I want to do
+
+I want to add add the H2 console to my Quarkus application
+
+# What did I do
+
+I looked at this [stack overflow article]https://stackoverflow.com/questions/61853691/how-to-set-h2-database-console-url-in-the-quarkus-application/64132441#64132441 on how to do it.
+
+It states that there are 2 things that need to be done
+
+* Add the `quarkus-vertx` and `quarkus-undertow` extensions which I did [build.gradle.kts](build.gradle.kts)
+* Create the deployment descriptor under src/main/resources/META-INF/web.xml [web.xml](src/main/resources/META-INF/web.xml) 
+
+During my gradle build I am getting the following error
+
+`Unsupported Java.
+Your build is currently configured to use Java 21.0.3 and Gradle 8.8.`
+
+I played around I am getting this exception as soon as I add the `quarkus-undertow` extensions.
+
+Do I have something wrong configured here or is there another way to achieve this?
+
---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-jdbc-h2")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    // Added these 2 libs
+    implementation("io.quarkus:quarkus-vertx")
+    implementation("io.quarkus:quarkus-undertow")
+
+}
+
+group = "com.chrispie"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
